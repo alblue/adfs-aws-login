cd0a28797f6f1dd05d71d5a24359fca6f270da04
Alex Blewitt <alex.blewitt@gmail.com>
2021-04-20 14:26:34 +0100

    Default username to empty string
    Fixes #4 by not throwing an error when a user name isn't specified, and allows the user to enter it.

--- adfs_aws_login/conf.py
+++ adfs_aws_login/conf.py
@@ -98,7 +98,7 @@ class Config:
         if not self.ROLE_ARN:
             self.ROLE_ARN = config_section.get("adfs_role_arn", "")
         if not self.DEFAULT_USERNAME:
-            self.DEFAULT_USERNAME = config_section.get("adfs_default_username", None)
+            self.DEFAULT_USERNAME = config_section.get("adfs_default_username", "")
         if not self.DURATION:
             conf_duration = config_section.get("adfs_session_duration", "")
             if conf_duration.isdigit():
